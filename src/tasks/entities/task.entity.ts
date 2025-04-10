a7121f1957dce00cc86e60aea020d798e169a39e
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2025-04-10 23:10:52 +0700

    feat: setup postgres

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,3 +1,5 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 export const TASK_STATUSES = [
   'TODO',
   'ON_PROGRESS',
@@ -7,14 +9,30 @@ export const TASK_STATUSES = [
 
 export type TaskStatus = (typeof TASK_STATUSES)[number];
 
+@Entity()
 export class Task {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 20, nullable: false })
   title: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
   description: string;
+
+  @Column({ type: 'enum', enum: TASK_STATUSES, default: 'TODO' })
   status: TaskStatus;
+
+  @Column({ type: 'bigint', nullable: false })
   ownerId: number;
+
+  @Column({ type: 'bigint', nullable: false })
   createdAt: number; //unix timestamp in milliseconds
+
+  @Column({ type: 'bigint', nullable: false })
   updatedAt: number; //unix timestamp in milliseconds
+
+  @Column({ type: 'boolean', default: false })
   isDeleted: boolean;
 
   constructor(id: number, title: string, description: string) {
