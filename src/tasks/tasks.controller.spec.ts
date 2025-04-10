4fc6bce369c3bdbebf19b017ae375ba9e6d9fd19
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2025-04-10 20:10:02 +0700

    add unit test on task controller and task service

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { Task } from './entities/task.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +15,57 @@ describe('TasksController', () => {
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+    service = module.get<TasksService>(TasksService);
   });
 
-  it('should be defined', () => {
+  it('controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('service should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('find all should return empty task', async () => {
+    jest.spyOn(service, 'findAll').mockImplementation(() => [] as Task[]);
+
+    const tasks = await controller.findAll();
+    expect(tasks).toHaveLength(0);
+  });
+
+  it('find all should return all task', async () => {
+    const tasks: Task[] = [
+      new Task(1, 'Task 1', 'Description 1'),
+      new Task(2, 'Task 2', 'Description 2'),
+    ];
+
+    jest.spyOn(service, 'findAll').mockImplementation(() => tasks);
+
+    const actual = await controller.findAll();
+    expect(actual).toBe(tasks);
+  });
+
+  it('find one should be return task', () => {
+    const task: Task = new Task(32, 'Task 1', 'Description 1');
+    jest
+      .spyOn(service, 'findOne')
+      .mockImplementation((id: number): Task | undefined => {
+        expect(id).toBe(task.id);
+        return task;
+      });
+
+    const actual = controller.findOne(32);
+    expect(actual).toBe(task);
+  });
+
+  it('find one should be thrown not found exeption ', () => {
+    jest
+      .spyOn(service, 'findOne')
+      .mockImplementation((id: number): Task | undefined => {
+        expect(id).toBe(32);
+        return undefined;
+      });
+
+    expect(() => controller.findOne(32)).toThrow(NotFoundException);
   });
 });
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 describe('TasksService', () => {
   let service: TasksService;
@@ -14,5 +15,19 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create should added new task', () => {
+    const dto: CreateTaskDto = {
+      title: 'Task 2',
+      description: 'Description 2',
+    };
+
+    const tasks = service.create(dto);
+    expect(tasks.id).toBe(3);
+    expect(tasks.title).toBe(dto.title);
+    expect(tasks.description).toBe(dto.description);
+    expect(tasks.isDeleted).toBe(false);
+    expect(service.findAll()).toHaveLength(2);
   });
 });
